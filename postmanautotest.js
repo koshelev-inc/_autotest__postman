7eedc31f87e8fe2405d753c716eb00fa6917e4cd
Mikhail Koshelev <74297925+koshelev-inc@users.noreply.github.com>
2023-10-21 12:02:38 +0700

    Update postmanautotest.js

--- postmanautotest.js
+++ postmanautotest.js
@@ -1,4 +1,19 @@
-// autotest for method api.hh.ru/employers/5008932
+
+
+
+pm.test("Мой первый автотест", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Body matches string Яндекс.Практикум", function () {
+    pm.expect(pm.response.text()).to.include("Яндекс.Практикум");
+});
+
+pm.test("Response time is less than 50ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(50);
+});
+
+
 
 
 pm.test("Мой первый автотест", function () {
@@ -13,18 +28,74 @@ pm.test("Response time is less than 50ms", function () {
     pm.expect(pm.response.responseTime).to.be.below(50);
 });
 
-
-//autotest for method https://api.hh.ru/employers?text=яндекс+практикум
-
-pm.test("Мой первый автотест", function () {
-    pm.response.to.have.status(200);
+pm.test("Extract and save ID", function () {
+    var jsonData = pm.response.json();
+    var id = jsonData.id;
+    
+    pm.globals.set("characterId", id);
 });
 
-pm.test("Body matches string Яндекс.Практикум", function () {
-    pm.expect(pm.response.text()).to.include("Яндекс.Практикум");
-});
+// Получаем ответ от сервера
+var responseBody = pm.response.json();
 
-pm.test("Response time is less than 50ms", function () {
-    pm.expect(pm.response.responseTime).to.be.below(50);
-});
+// Проверяем наличие и тип свойств
+if (Array.isArray(responseBody.pokemons_alive) && Array.isArray(responseBody.pokemons_in_pokeballs)) {
+    // Получаем массив ID покемонов, находящихся в покеболлах
+    var pokemonsInPokeballsIds = responseBody.pokemons_in_pokeballs.map(pokemon => pokemon.id);
+
+    // Фильтруем массив живых покемонов, чтобы найти отсутствующие
+    var missingPokemonIds = responseBody.pokemons_alive.filter(alivePokemonId => !pokemonsInPokeballsIds.includes(alivePokemonId));
+
+    // Сохраняем строку отсутствующих покемонов в глобальные параметры Postman
+    pm.globals.set("missingPokemonIds", missingPokemonIds.join(','));
+} else {
+    console.log("Required properties are missing or not in the expected format.");
+}
+
+
+var users = pm.response.json(); 
+function shouldExcludeUser(user) {
+    return user.trainer_name === "nagibator";
+}
+
+
+function shouldExcludePokemon(pokemon, excludedIds) {
+    return excludedIds.includes(pokemon.id);
+}
+
+
+var excludedUserIds = pm.globals.get("excludedUserIds") || "";
+
+
+var foundPokemonId = null;
+
+for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    
+    if (shouldExcludeUser(user)) {
+        continue; // Пропускаем исключенных пользователей
+    }
+
+    var pokemonsInPokeballs = user.pokemons_in_pokeballs;
+
+    var excludedPokemonIds = excludedUserIds.split(",");
+
+ 
+    var filteredPokemons = pokemonsInPokeballs.filter(function(pokemon) {
+        return !shouldExcludePokemon(pokemon, excludedPokemonIds);
+    });
+
+    if (filteredPokemons.length > 0) {
+        foundPokemonId = filteredPokemons[0].id;
+        excludedUserIds += "," + user.id; 
+        break; 
+    }
+}
+
+
+pm.globals.set("foundPokemonId", foundPokemonId);
+pm.globals.set("excludedUserIds", excludedUserIds);
+
+
+
 
